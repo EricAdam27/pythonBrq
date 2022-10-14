3c5f81f02d11ba6a2af659d84a268da4b3d9a731
Eric Adam <eadamfs.programador@gmail.com>
2022-10-14 17:02:38 -0300

    refatorando atividade 3

--- 3a.py
+++ 3a.py
@@ -1,13 +1,26 @@
+def numeroDeArvoresQuandoSeDesceNoAngulo(angulo):
+    variacaoHorizontal, variacaoVertical = angulo
+
+    numeroDeArvores = 0
+    posicaoHorizontal = 0
+
+    for posicaoVertical in range(0, len(mapa), variacaoVertical):
+        linha = mapa[posicaoVertical]
+        tamanhoDalinha = len(linha)
+        if linha[posicaoHorizontal % tamanhoDalinha] == '#':
+            numeroDeArvores += 1
+        posicaoHorizontal += variacaoHorizontal
+
+    return numeroDeArvores
+
+
 with open('3input.txt') as file:
     mapa = file.read().splitlines()
 
-numeroDeArvores = 0
-posicaoHorizontal = 0
+angulos = ((1, 1), (3, 1), (5, 1), (7, 1), (1, 2),)
+multiplicacaoDoNumeroDeCadaAngulo = 1
 
-for linha in mapa:
-    tamanhoDalinha = len(linha)
-    if linha[posicaoHorizontal % tamanhoDalinha] == '#':
-        numeroDeArvores += 1
-    posicaoHorizontal += 3
+for angulo in angulos:
+    multiplicacaoDoNumeroDeCadaAngulo *= numeroDeArvoresQuandoSeDesceNoAngulo(angulo)
 
-print(numeroDeArvores)
+print(multiplicacaoDoNumeroDeCadaAngulo)
