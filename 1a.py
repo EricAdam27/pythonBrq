0c5680e31ba8b46f608cbbc7128fbc6ddf076eb8
Eric Adam <eadamfs.programador@gmail.com>
2022-10-14 15:02:19 -0300

    atividade 1

---
+++ 1a.py
@@ -0,0 +1,8 @@
+with open('1input.txt') as file:
+    listaDosNumeros = file.read().splitlines()
+    listaDosNumeros = list(map(int, listaDosNumeros))
+
+for elemento1 in listaDosNumeros:
+    for elemento2 in listaDosNumeros:
+        if elemento1 + elemento2 == 2020:
+            print(elemento1 * elemento2)
