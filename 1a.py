6e475fadc32474c28f9a1f2735189c8650f8740b
Eric Adam <eadamfs.programador@gmail.com>
2022-10-14 15:15:12 -0300

    refatorando atividade 1

--- 1a.py
+++ 1a.py
@@ -2,7 +2,9 @@ with open('1input.txt') as file:
     listaDosNumeros = file.read().splitlines()
     listaDosNumeros = list(map(int, listaDosNumeros))
 
-for elemento1 in listaDosNumeros:
-    for elemento2 in listaDosNumeros:
-        if elemento1 + elemento2 == 2020:
-            print(elemento1 * elemento2)
+resultado = [elemento1 * elemento2
+             for elemento1 in listaDosNumeros
+             for elemento2 in listaDosNumeros
+             if elemento1 + elemento2 == 2020]
+
+print(resultado[0])
